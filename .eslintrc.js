46df869114f2226b0e120dd255bc3bb8ff67ba9e
Wallace Vieira <wallaceofc@hotmail.com>
2024-01-23 07:19:19 -0300

    feat: creating repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,42 @@
 module.exports = {
-	"env": {
-		"es2021": true,
-		"node": true
+	env: {
+		es2021: true,
+		node: true,
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended"
-	],
-	"overrides": [
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true,
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	"plugins": [
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
+	plugins: ["@typescript-eslint"],
+	rules: {
+		camelcase: "off",
+		"@typescript-eslint/naming-convention": [
 			"error",
-			"tab"
+			{
+				selector: "interface",
+				format: ["PascalCase"],
+				custom: {
+					regex: "^I[A-Z]",
+					match: true,
+				},
+			},
 		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
