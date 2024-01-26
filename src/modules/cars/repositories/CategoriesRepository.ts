90e81d88de2d5c00e3b92d83167a1abdfa66a0c0
Wallace Vieira <wallaceofc@hotmail.com>
2024-01-26 12:31:14 -0300

    feat: singleton pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,8 +4,17 @@ import { ICreateCategoryDTO } from "./ICategoriesRepostory";
 class CategoriesRepository {
 	private  categories: Category[];
 
-	constructor() {
+	private static INSTANCE: CategoriesRepository;
+
+	private constructor() {
 		this.categories = [];
+	}
+
+	public static getInstance(): CategoriesRepository {
+		if (!CategoriesRepository.INSTANCE) {
+			CategoriesRepository.INSTANCE = new CategoriesRepository();
+		}
+		return CategoriesRepository.INSTANCE;
 	}
 
 	create({ description, name }: ICreateCategoryDTO): void {
